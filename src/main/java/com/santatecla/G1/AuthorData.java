45d4b66f5934951927a929a7851d929c768acbbe
RubenBernabe <37367277+RubenBernabe@users.noreply.github.com>
2019-02-12 21:26:53 +0100

    Inicios BBDD Author

--- src/main/java/com/santatecla/G1/AuthorData.java
+++ src/main/java/com/santatecla/G1/AuthorData.java
@@ -1,11 +1,24 @@
 package com.santatecla.G1;
 
 import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class AuthorData {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
 	private String name;
 	private String urlImage;
 	private Date bornDate, deathDate;
 	private String bornPlace;
 	private String urlMap;
+	
+	//Constructor to Spring
+	protected AuthorData() {}
+	
+	
 }
---
+++ src/main/java/com/santatecla/G1/AuthorDataRepository.java
@@ -0,0 +1,7 @@
+package com.santatecla.G1;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorDataRepository extends JpaRepository<AuthorData,Long>{
+
+}
