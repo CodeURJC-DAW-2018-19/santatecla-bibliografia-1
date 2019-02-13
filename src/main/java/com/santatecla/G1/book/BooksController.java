57ecad2f45d15af99c2425d57bdc27dcf0ac6654
RubenBernabe <37367277+RubenBernabe@users.noreply.github.com>
2019-02-13 17:01:26 +0100

    BBDD Author and Book

--- src/main/java/com/santatecla/G1/book/BooksController.java
+++ src/main/java/com/santatecla/G1/book/BooksController.java
@@ -1,27 +1,44 @@
 package com.santatecla.G1.book;
 
+import java.util.Collection;
+
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BooksController {
-	
+
 	@Autowired
 	private BookRepository repository;
-	
+
 	@PostConstruct
 	public void init() {
 		repository.save(new Book("Pepe","mama"));
 		repository.save(new Book("Juan","mama"));
 	}
 	
-	@RequestMapping("/books")
-	public String author(Model model) {
-		
-		return "booksPage";
+	public Collection<Book> books(){
+		return repository.findAll();
 	}
+	
+	@RequestMapping("/book/{id}")
+	public String Author(Model model, @PathVariable long id) {
+		Book book = repository.findOne(id);
+		if (book!=null) {
+			model.addAttribute("book", book);
+		}
+		return "bookPage";
+	}
+	
+	@RequestMapping("/book")
+	public String newAuthor(Model model, Book book) {
+		repository.save(book);
+		return "bookPage";
+	}
+
 }
