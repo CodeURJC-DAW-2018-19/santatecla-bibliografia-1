c4e25221a246d836446e1d8034adfda86d1747a4
Rau <r.martinpin@alumnos.urjc.es>
2019-02-23 19:44:21 +0100

    User changes

--- src/main/java/com/santatecla/G1/book/BooksController.java
+++ src/main/java/com/santatecla/G1/book/BooksController.java
@@ -1,17 +1,24 @@
 package com.santatecla.G1.book;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.santatecla.G1.user.UserComponent;
 
 @Controller
 public class BooksController {
 
+	@Autowired
+	private UserComponent userComponent;
+	
 	@Autowired
 	private BookRepository repository;
 	
@@ -34,5 +41,15 @@ public class BooksController {
 		//repository.save(book);
 		return "booksPage";
 	}
+	
+	@ModelAttribute
+	public void addUserToModel(Model model) {
+		boolean logged = userComponent.getLoggedUser() != null;
+		if(logged) {
+			model.addAttribute("admin", userComponent.getLoggedUser().getRoles().contains("ROLE_ADMIN"));
+			//model.addAttribute("userName",userComponent.getLoggedUser().getName());
+		}
+	}
+	
 
 }
--- src/main/java/com/santatecla/G1/theme/ThemeController.java
+++ src/main/java/com/santatecla/G1/theme/ThemeController.java
@@ -1,15 +1,19 @@
 package com.santatecla.G1.theme;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.santatecla.G1.book.Book;
+import com.santatecla.G1.citation.Citation;
+import com.santatecla.G1.user.UserComponent;
 
 
 @Controller
@@ -17,15 +21,18 @@ public class ThemeController {
 	
 	@Autowired
 	private ThemeRepository repository;
-
+	@Autowired
+	private UserComponent userComponent;
 	
 	
 	@RequestMapping("/theme/{id}")
 	public String theme(Model model, @PathVariable long id) {
-		Optional<Theme> theme = repository.findById(id);
+		Theme theme = repository.findById(id);
+		List<Citation> citations = repository.findCitationByName(theme.getName());
 		System.out.println(theme.toString());
 		if (theme!=null) {
-			model.addAttribute("theme", theme.get());
+			model.addAttribute("theme", theme);
+			model.addAttribute("citations",citations);
 		}
 		return "themePageEdit";
 	}
@@ -37,6 +44,15 @@ public class ThemeController {
 	
 	public Collection<Theme> themes(){
 		return repository.findAll();
+	}
+	@ModelAttribute
+	public void addUserToModel(Model model) {
+		boolean logged = userComponent.getLoggedUser() != null;
+		model.addAttribute("logged", logged);
+		if(logged) {
+			model.addAttribute("admin", userComponent.getLoggedUser().getRoles().contains("ROLE_ADMIN"));
+			//model.addAttribute("userName",userComponent.getLoggedUser().getName());
+		}
 	}
 	
 	
