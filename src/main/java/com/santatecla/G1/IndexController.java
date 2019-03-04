b7a9923f188d34688b7b6b643a637fe7f9a55a9b
Natalia <natalia.madruenno@gmail.com>
2019-03-04 12:08:28 +0100

    added PageRequest and pageable html

--- src/main/java/com/santatecla/G1/IndexController.java
+++ src/main/java/com/santatecla/G1/IndexController.java
@@ -1,6 +1,11 @@
 package com.santatecla.G1;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,9 +41,9 @@ public class IndexController {
 	@RequestMapping("/")
 	public String author(Model model) {
 		
-		model.addAttribute("authors",authorRepository.findAll());
-		model.addAttribute("books",booksRepository.findAll());
-		model.addAttribute("themes",themesRepository.findAll());
+		model.addAttribute("authors",authorRepository.findAll(new PageRequest(0, 10)));
+		model.addAttribute("books",booksRepository.findAll(new PageRequest(0, 10)));
+		model.addAttribute("themes",themesRepository.findAll(new PageRequest(0, 10)));
 		model.addAttribute("images",imageController.getImages().values());
 		
 		return "indexPage";
