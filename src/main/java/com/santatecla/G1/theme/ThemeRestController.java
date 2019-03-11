c52de5deab9640bc0960dcc8257c1d95c05c7e6b
RodriLS <37367668+RodriLS@users.noreply.github.com>
2019-03-11 02:37:17 +0100

    Revert "fixed error theme post"
    This reverts commit 4e72e82cd3d184e756085856458d0ca99a492fa6.

--- src/main/java/com/santatecla/G1/theme/ThemeRestController.java
+++ src/main/java/com/santatecla/G1/theme/ThemeRestController.java
@@ -62,7 +62,6 @@ public class ThemeRestController {
 				for (Book book : theme.getBook()) {
 
 					books.add(bookService.findById(book.getId()));
-					themeService.save(theme);
 					book.setTheme(theme);
 					bookService.save(book);
 				}
