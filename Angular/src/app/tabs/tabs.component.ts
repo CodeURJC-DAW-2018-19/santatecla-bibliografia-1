4422884d56e7a7190fb3fc5ba52587beedbbb44e
Natalia <natalia.madruenno@gmail.com>
2019-03-26 10:37:54 +0100

    added methods tabs

--- Angular/src/app/tabs/tabs.component.ts
+++ Angular/src/app/tabs/tabs.component.ts
@@ -10,15 +10,27 @@ export class TabsComponent implements OnInit {
   private nTabs: Tab[] = [];
   
 
-  addTabs(){
+  exampleTabs(){
     this.nTabs.push(new Tab("Tab1", "fdsfs"));
     this.nTabs.push(new Tab("Tab2", "fdsfs"));
     this.nTabs.push(new Tab("Tab3", "fdsfs"));
     this.nTabs.push(new Tab("Tab4", "fdsfs"));
   }
 
+  removeTab(name:string){
+    for (var i=0; i<this.nTabs.length; i++){
+      if (this.nTabs[i].Name === name){
+        this.nTabs.splice(i);
+      }
+    }
+  }
+
+  addTab(name:string, url:string){
+    this.nTabs.push(new Tab("name", "url"));
+  }
+
   ngOnInit() {
-    this.addTabs();
+    this.exampleTabs();
   }
 }
 
