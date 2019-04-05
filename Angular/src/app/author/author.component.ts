de1a9c7f259218c7541e81bb9239fb2fc82dffaa
Unknown <natalia.madruenno@gmail.com>
2019-04-05 14:28:41 +0200

    fixed tabs author

--- Angular/src/app/author/author.component.ts
+++ Angular/src/app/author/author.component.ts
@@ -5,67 +5,73 @@ import { Author, AuthorService } from './author.service';
 import { LoginService } from '../login/login.service';
 import { TdDialogService } from '@covalent/core';
 import { Book } from '../book/book.service';
-import { Theme} from '../theme/theme.service';
+import { Theme } from '../theme/theme.service';
 import { ThemeColumnComponent } from '../theme/themeColumn.component';
 import { Citation } from '../citation/citation.service';
-
+import { TabsService } from '../tabs/tabs.service';
 @Component({
-  templateUrl: './author.component.html',
+    templateUrl: './author.component.html',
 })
-export class AuthorComponent implements OnInit{
+export class AuthorComponent implements OnInit {
 
     author: Author;
-    books:Book[];
+    books: Book[];
     themes: Theme[];
-    citations:Citation[]
+    citations: Citation[]
 
     constructor(
-        private router: Router, 
-        public activatedRoute: ActivatedRoute, 
+        private router: Router,
+        public activatedRoute: ActivatedRoute,
         public service: AuthorService,
         public loginService: LoginService,
-        private _dialogService: TdDialogService,) {this.themes=[]; this.citations=[];this.books=[]}
+        private _dialogService: TdDialogService,
+        private tabsService: TabsService )
+         { this.themes = []; this.citations = []; this.books = [] }
 
     ngOnInit() {
-        const id = this.activatedRoute.snapshot.params['id'];
-        var aux:Citation[];
-        this.service.getAuthor(id).subscribe(
-            author => {this.author = author
-            this.books=this.author.books
-            this.books.forEach(book=>{
-                this.themes.push(book.theme)
-                aux=book.citation
-                aux.forEach(cit=>{
-                    this.citations.push(cit)
-                })
-            })
-            console.log(this.author)
-        },
-            error => console.error(error)
-        );
-        console.log("Current location:", this.router.url);
+        this.activatedRoute.params.subscribe(
+            params => {
+                const id = params['id'];
+                var aux: Citation[];
+                this.service.getAuthor(id).subscribe(
+                    author => {
+                    this.author = author
+                        this.books = this.author.books
+                        this.books.forEach(book => {
+                            this.themes.push(book.theme)
+                            aux = book.citation
+                            aux.forEach(cit => {
+                                this.citations.push(cit)
+                            })
+                        })
+                        this.tabsService.addTab(this.author.name, this.router.url)
+                    },
+                    error => console.error(error)
+                );
+            }
+        )
     }
 
-    saveAuthor(author:Author) {
+    saveAuthor(author: Author) {
         console.log(author)
         this.service.saveAuthor(author).subscribe(
-            _ => {},
+            _ => { },
             (error: Error) => console.error('Error updating an author: ' + error),
-        ); 
+        );
         window.history.back();
     }
-    
-    deleteAuthor(author:Author) {
+
+    deleteAuthor(author: Author) {
         this._dialogService.openConfirm({
             message: '¿Quiéres borrar este autor?',
-            title: 'Confirmar', 
-            width: '500px', 
+            title: 'Confirmar',
+            width: '500px',
             height: '175px'
         }).afterClosed().subscribe((accept: boolean) => {
             if (accept) {
                 this.service
                     .deleteAuthor(author)
-                    .subscribe((_) => {}, (error) => console.error(error));
+                    .subscribe((_) => { }, (error) => console.error(error));
             }
             window.history.back();
         });
