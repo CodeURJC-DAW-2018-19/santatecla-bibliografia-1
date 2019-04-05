ece065301001e6b2a34f4394163fac31ce6da813
RubenBernabe <37367277+RubenBernabe@users.noreply.github.com>
2019-04-05 16:20:10 +0200

    add dinamic citationsForms
    Error when the length citations[] is big

--- Angular/src/app/theme/themeDetail.component.ts
+++ Angular/src/app/theme/themeDetail.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Theme, ThemeService } from './theme.service';
 import { LoginService } from '../login/login.service';
 import { TabsService } from "../tabs/tabs.service";
+import { Citation } from '../citation/citation.service';
 
 @Component({
     templateUrl: './themeDetail.component.html',
@@ -12,6 +13,8 @@ import { TabsService } from "../tabs/tabs.service";
 export class ThemeDetailComponent {
     @Input()
     theme: Theme;
+    @Input()
+    citations:Citation[]
 
     constructor(
         private router: Router,
